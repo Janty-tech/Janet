c7e361fe14587d7d135c296563c1604bcd1b836e
Janty-tech <kiriamajanet640@gmail.com>
2023-04-04 15:16:02 +0300

    Thisd is programming

---
+++ functions.py
@@ -0,0 +1,34 @@
+#Thisd is the print fuction
+"""This is the print function"""
+print("Hello World")
+print(2)
+print(3.4)ja
+
+
+#variables
+x=2
+y=3
+print(x)
+print(y)
+print(x+y)
+print(x*y)
+
+name = input("what is your name?")
+print(name)
+
+first number = int(input("what is your first")
+second number =int(input("what is your second")
+print(first_ number*second number)
+
+number one = int(input("what is your first")
+number two =int(input("what is your second")
+print(f{nunmber_one *number two})
+
+#variables'
+Z= float(input("what is your Z?"))
+Y= float(input("what is your y?"))
+print(Z*Y)
+
+
+
+
